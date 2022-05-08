dec5ab7b37afad7d06422bb6a7797bb683bb9bff
clemins10 <103154610+clemins10@users.noreply.github.com>
2022-05-08 17:15:47 -0400

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
-
+//Clemins Njong
+//cnjong1@umbc.edu
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        //Part 1 Method
+        int number1 = 10, number2 = 5;
+        System.out.println("Add : 10 + 5 = " + Add(number1,number2));
+        System.out.println("Subtraction : 10 - 5 = " + Subtraction(number1,number2));
+        System.out.println("Multiplication : 10 * 5 = " + Multiplication(number1,number2));
+        System.out.println("Division : 10/5 = " + Division(number1,number2));
+    }
+    public static int Add(int number1, int number2)
+    {    return number1+number2;
+    }
+    public static int Subtraction(int number1, int number2)
+    {    return number1-number2;
+    }
+    public static int Multiplication(int number1, int number2)
+    {    return number1*number2;
+    }
+    public static int Division(int number1, int number2)
+    {    return number1/number2;
     }
 }
